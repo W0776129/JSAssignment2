084d038f040a843ad9fec5d81ee92c93cb269feb
Nikhil Patel <90627433+W0776129@users.noreply.github.com>
2021-10-03 23:03:32 -0400

    as3 done

--- WEB303As03/script.js
+++ WEB303As03/script.js
@@ -1,4 +1,11 @@
-$(function () {
+//Assignment 3
+//Aauthor: Nikhil Patel
+//Date: 03 Oct 2021
+
+
+//method 1
+
+/*$(document).ready(function () {
    
     let getJSONObject = $.getJSON("team.json", function (data) {
         console.log("data from my getJSON method", data);
@@ -11,7 +18,35 @@ $(function () {
             
         });
         $(`#team`).html(temp);
-    }).always(function (data, status, error) {
-        console.log("data", data, "status", status, "error", error);
-    });
+    }
+});
+
+*/
+
+
+//method 2
+
+$(document).ready(function () {
+    setTimeout(loadJson, 5000);  // MAKE THE AJAX CALL AFTER A FEW SECONDS DELAY.
+    $(`#team`).html('<p>Loading</p>');
+        function loadJson() {
+            $.ajax({
+                type: 'GET',
+                url: 'team.json',
+                dataType: 'json',
+                success: function (data) {
+                    let temp= '';
+                    $.each(data.teammembers, function (key, val) {
+                       temp += '<h3>' + val.name + '</h3>';
+                       temp += '<h4>' + val.title + '</h4>';
+                       temp += '<p>' + val.bio + '</p>';
+                        
+                    });
+                    $(`#team`).html(temp);
+                },
+                error: function(){
+                    console.log("error");
+                }
+            });
+        }
 });
